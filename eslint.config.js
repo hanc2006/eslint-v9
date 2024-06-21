2bcc612835fa4c3b3c1ab1fb5e654ce196bbef9e
damiano.barbati <damiano.barbati@gmail.com>
2024-06-21 21:41:46 +0200

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import tslint from 'typescript-eslint';
 import { importConfig } from './src/configs/import.js';
 import { prettierConfig } from './src/configs/prettier.js';
 import { reactConfig } from './src/configs/react.js';
-import { stylisticConfig } from './src/configs/stylistic.js';
 import { tailwindConfig } from './src/configs/tailwind.js';
 import { unicornConfig } from './src/configs/unicorn.js';
 
@@ -31,17 +30,16 @@ export default tslint.config(
   ...(await prettierConfig),
   ...(await unicornConfig),
   ...(await importConfig),
-  ...(await stylisticConfig),
   ...(await tailwindConfig),
   {
     rules: {
       eqeqeq: 'error',
-      // '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/consistent-type-assertions': 'off',
-      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-      '@typescript-eslint/ban-types': 'error',
-      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true, argsIgnorePattern: '^_' }],
+      // '@typescript-eslint/await-thenable': 'error',
+      // '@typescript-eslint/consistent-type-assertions': 'off',
+      // '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      // '@typescript-eslint/ban-types': 'error',
+      // '@typescript-eslint/ban-ts-comment': 'off',
     },
   },
 );
